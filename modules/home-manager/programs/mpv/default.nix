72f63cfbad506bea5084236291b17627706d0149
chtc <chtc@tuta.io>
2024-02-07 13:38:28 +0100

    feat(mpv): add missing configuration options

--- modules/home-manager/programs/mpv/default.nix
+++ modules/home-manager/programs/mpv/default.nix
@@ -5,9 +5,12 @@
       save-position-on-quit = true;
       hwdec = "auto-copy";
       hwdec-codecs = "all";
+      hr-seek-framedrop="no";
+      gpu-api="vulkan";
+      vulkan-swap-mode="mailbox";
       deband = "yes";
       deband-iterations = 3;
-      deband-treshhold = 45;
+      deband-threshhold = 45;
       deband-range = 25;
       deband-grain = 15;
       dither-depth = "auto";
