b0297f09b41227c38a658c4cb2f32911215d6f92
chtc <chtc@tuta.io>
2024-02-09 15:41:48 +0100

    fix(mpv): fix typo in deband-threshold

--- modules/home-manager/programs/mpv/default.nix
+++ modules/home-manager/programs/mpv/default.nix
@@ -9,7 +9,7 @@
       gpu-context = "wayland";
       deband = "yes";
       deband-iterations = 3;
-      deband-threshhold = 45;
+      deband-threshold = 45;
       deband-range = 25;
       deband-grain = 15;
       dither-depth = "auto";
