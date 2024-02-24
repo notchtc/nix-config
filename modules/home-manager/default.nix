8e018c13439835b7926e7451ef71a0a0213417fe
chtc <chtc@tuta.io>
2024-02-24 16:47:22 +0100

    use SRI hash for LS_COLORS

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -69,8 +69,8 @@ in {
       enable = true;
 
       extraConfig = builtins.readFile (pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/master/LS_COLORS";
-        hash = "sha256-1a9pvv8kgy7cx0vkxyr7lvq2hs04pkg9nql73za9y39s2w8gyj3d";
+        url = "https://raw.githubusercontent.com/trapd00r/LS_COLORS/a283d79dcbb23a8679f4b1a07d04a80cab01c0ba/LS_COLORS";
+        hash = "sha256-bUj/EBc6DZ/UH4dim968BGgo8KYn+z436Oz4N9HeN6k=";
       });
     };
 
