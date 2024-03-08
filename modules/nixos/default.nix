875fb077435918fc1583409ca9e3a1f8c7a3124c
chtc <chtc@tuta.io>
2024-03-08 20:44:17 +0100

    feat: limit number of locales

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -45,6 +45,10 @@
 
   i18n = {
     defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "pl_PL.UTF-8/UTF-8"
+    ];
     extraLocaleSettings = {
       LC_TIME = "pl_PL.UTF-8";
       LC_MONETARY = "pl_PL.UTF-8";
