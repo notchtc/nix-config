b4aa02a49599846925ab58b66fbe09864d6c4ddf
chtc <chtc@tuta.io>
2024-03-20 09:46:51 +0100

    fix: pinentry conflict between xserver and plasma6

--- modules/nixos/desktops/plasma6.nix
+++ modules/nixos/desktops/plasma6.nix
@@ -16,6 +16,8 @@
     desktopManager.plasma6.enable = true;
   };
 
+  programs.gnupg.agent.pinentryPackage = lib.mkForce pkgs.pinentry-qt;
+
   environment.plasma6.excludePackages = lib.attrValues {
     inherit (pkgs.kdePackages) elisa kate konsole;
   };
