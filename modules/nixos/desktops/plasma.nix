c360c10e17460328df867834f6a50b34c4e041e0
chtc <chtc@tuta.io>
2024-04-27 12:52:14 +0200

    feat(plasma): switch to tty 7 to make booting more seamless

--- modules/nixos/desktops/plasma.nix
+++ modules/nixos/desktops/plasma.nix
@@ -17,6 +17,8 @@
     desktopManager.plasma6.enable = true;
   };
 
+  systemd.services.plymouth-quit.preStart = "${pkgs.kbd}/bin/chvt 7";
+
   environment.plasma6.excludePackages = lib.attrValues {
     inherit (pkgs.kdePackages) elisa kate konsole;
   };
