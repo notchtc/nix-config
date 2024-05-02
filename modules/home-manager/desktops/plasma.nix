d1a191ab7af812095686be3e32c97a9988d1998f
chtc <chtc@tuta.io>
2024-05-02 16:08:54 +0200

    refactor(plasma): don't use .value

--- modules/home-manager/desktops/plasma.nix
+++ modules/home-manager/desktops/plasma.nix
@@ -94,66 +94,64 @@ in
     };
 
     configFile = {
-      "kdeglobals"."General"."XftHintStyle".value = "hintslight";
-      "kdeglobals"."General"."XftSubPixel".value = "rgb";
-      "kdeglobals"."General"."fixed".value = "Iosevka Nerd Font Mono,10,-1,5,50,0,0,0,0,0";
-      "kdeglobals"."General"."font".value = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
-      "kdeglobals"."General"."menuFont".value = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
-      "kdeglobals"."General"."smallestReadableFont".value = "Iosevka Nerd Font,8,-1,5,50,0,0,0,0,0";
-      "kdeglobals"."General"."toolBarFont".value = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
-      "kdeglobals"."General"."TerminalApplication".value = "foot";
-      "kdeglobals"."General"."TerminalService".value = "org.codeberg.dnkl.foot.desktop";
-      "kdeglobals"."KDE"."AnimationDurationFactor".value = 0;
-      "kdeglobals"."WM"."activeFont".value = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."XftHintStyle" = "hintslight";
+      "kdeglobals"."General"."XftSubPixel" = "rgb";
+      "kdeglobals"."General"."fixed" = "Iosevka Nerd Font Mono,10,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."font" = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."menuFont" = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."smallestReadableFont" = "Iosevka Nerd Font,8,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."toolBarFont" = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
+      "kdeglobals"."General"."TerminalApplication" = "foot";
+      "kdeglobals"."General"."TerminalService" = "org.codeberg.dnkl.foot.desktop";
+      "kdeglobals"."KDE"."AnimationDurationFactor" = 0;
+      "kdeglobals"."WM"."activeFont" = "Iosevka Nerd Font,10,-1,5,50,0,0,0,0,0";
 
-      "kxkbrc"."Layout"."LayoutList".value = "pl";
-      "kxkbrc"."Layout"."Options".value = "caps:swapescape";
+      "kxkbrc"."Layout"."LayoutList" = "pl";
+      "kxkbrc"."Layout"."Options" = "caps:swapescape";
 
-      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."Image".value = "${wallpaperImg}";
-      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."PreviewImage".value = "${
-        wallpaperImg
-      }";
+      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."Image" = "${wallpaperImg}";
+      "kscreenlockerrc"."Greeter/Wallpaper/org.kde.image/General"."PreviewImage" = "${wallpaperImg}";
 
-      "kwinrc"."Compositing"."LatencyPolicy".value = "Low";
-      "kwinrc"."NightColor"."Active".value = true;
-      "kwinrc"."NightColor"."NightTemperature".value = 3000;
-      "kwinrc"."ModifierOnlyShortcuts"."Meta".value = "org.kde.krunner,/App,,toggleDisplay";
-      "kwinrc"."Desktops"."Number".value = 4;
-      "kwinrc"."Desktops"."Rows".value = 2;
-      "kwinrc"."Plugins"."blurEnabled".value = false;
+      "kwinrc"."Compositing"."LatencyPolicy" = "Low";
+      "kwinrc"."NightColor"."Active" = true;
+      "kwinrc"."NightColor"."NightTemperature" = 3000;
+      "kwinrc"."ModifierOnlyShortcuts"."Meta" = "org.kde.krunner,/App,,toggleDisplay";
+      "kwinrc"."Desktops"."Number" = 4;
+      "kwinrc"."Desktops"."Rows" = 2;
+      "kwinrc"."Plugins"."blurEnabled" = false;
 
-      "krunnerrc"."General"."FreeFloating".value = true;
-      "krunnerrc"."Plugins"."baloosearchEnabled".value = false;
-      "krunnerrc"."Plugins"."browserhistoryEnabled".value = false;
-      "krunnerrc"."Plugins"."browsertabsEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_appstreamEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_bookmarksrunnerEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_charrunnerENabled".value = false;
-      "krunnerrc"."Plugins"."krunner_dictionaryEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_katesessionsEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_konsoleprofilesEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_sessionsEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_spellcheckEnabled".value = false;
-      "krunnerrc"."Plugins"."krunner_webshortcutsEnabled".value = false;
-      "krunnerrc"."Plugins"."org.kde.activities2Enabled".value = false;
-      "krunnerrc"."Plugins/Favorites"."plugins".value = "krunner_services";
+      "krunnerrc"."General"."FreeFloating" = true;
+      "krunnerrc"."Plugins"."baloosearchEnabled" = false;
+      "krunnerrc"."Plugins"."browserhistoryEnabled" = false;
+      "krunnerrc"."Plugins"."browsertabsEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_appstreamEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_bookmarksrunnerEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_charrunnerENabled" = false;
+      "krunnerrc"."Plugins"."krunner_dictionaryEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_katesessionsEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_konsoleprofilesEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_sessionsEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_spellcheckEnabled" = false;
+      "krunnerrc"."Plugins"."krunner_webshortcutsEnabled" = false;
+      "krunnerrc"."Plugins"."org.kde.activities2Enabled" = false;
+      "krunnerrc"."Plugins/Favorites"."plugins" = "krunner_services";
 
-      "kwalletrc"."Wallet"."Close When Idle".value = true;
-      "kwalletrc"."Wallet"."Close on Screensaver".value = false;
-      "kwalletrc"."Wallet"."Default Wallet".value = "kdewallet";
-      "kwalletrc"."Wallet"."Enabled".value = true;
-      "kwalletrc"."Wallet"."First Use".value = false;
-      "kwalletrc"."Wallet"."Idle Timeout".value = 5;
-      "kwalletrc"."Wallet"."Launch Manager".value = false;
-      "kwalletrc"."Wallet"."Leave Manager Open".value = false;
-      "kwalletrc"."Wallet"."Leave Open".value = false;
-      "kwalletrc"."Wallet"."Prompt on Open".value = false;
-      "kwalletrc"."Wallet"."Use One Wallet".value = true;
-      "kwalletrc"."org.freedesktop.secrets"."apiEnabled".value = true;
+      "kwalletrc"."Wallet"."Close When Idle" = true;
+      "kwalletrc"."Wallet"."Close on Screensaver" = false;
+      "kwalletrc"."Wallet"."Default Wallet" = "kdewallet";
+      "kwalletrc"."Wallet"."Enabled" = true;
+      "kwalletrc"."Wallet"."First Use" = false;
+      "kwalletrc"."Wallet"."Idle Timeout" = 5;
+      "kwalletrc"."Wallet"."Launch Manager" = false;
+      "kwalletrc"."Wallet"."Leave Manager Open" = false;
+      "kwalletrc"."Wallet"."Leave Open" = false;
+      "kwalletrc"."Wallet"."Prompt on Open" = false;
+      "kwalletrc"."Wallet"."Use One Wallet" = true;
+      "kwalletrc"."org.freedesktop.secrets"."apiEnabled" = true;
 
-      "baloofilerc"."Basic Settings"."Indexing-Enabled".value = false;
+      "baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
 
-      "plasmanotifyrc"."Notifications"."PopupPosition".value = "TopRight";
+      "plasmanotifyrc"."Notifications"."PopupPosition" = "TopRight";
     };
   };
 }
