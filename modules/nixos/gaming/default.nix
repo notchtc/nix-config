de3a8fb8ec8f8856d7aa1902821dce7d3ba26387
chtc <chtc@tuta.io>
2024-08-02 21:06:56 +0200

    feat(gaming): switch to gamemode
    refactor(gaming): place everything in one file

--- modules/nixos/gaming/default.nix
+++ modules/nixos/gaming/default.nix
@@ -1,13 +1,36 @@
 { pkgs, ... }:
 {
-  imports = [
-    ./ananicy.nix
-    ./steam.nix
-  ];
-
   environment.systemPackages = with pkgs; [
     lime3ds
     melonDS
     ppsspp-sdl-wayland
+    wineWowPackages.stagingFull
   ];
+
+  programs = {
+    gamemode = {
+      enable = true;
+      settings = {
+        general = {
+          softrealtime = "auto";
+          renice = 15;
+        };
+      };
+    };
+
+    steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      extest.enable = true;
+
+      extraCompatPackages = [ pkgs.proton-ge-custom ];
+      extraPackages = with pkgs; [
+        protontricks
+        steamtinkerlaunch
+        winetricks
+      ];
+    };
+  };
 }
