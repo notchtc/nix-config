f49a93832ab8d1240160d41e7b4eb1f3dc39923a
chtc <chtc@tuta.io>
2024-12-21 17:17:38 +0100

    feat(laptop): make tlp set scaling governor to performance on AC

--- modules/nixos/laptop/default.nix
+++ modules/nixos/laptop/default.nix
@@ -14,7 +14,7 @@
       CPU_HWP_DYN_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
     };
   };
 }
