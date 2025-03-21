630df1ab77f4726f4a0038b3ed7f1ed5fa254d6a
chtc <chtc@tuta.io>
2025-03-21 14:55:07 +0100

    feat(gnome): set keyboard layout

--- modules/home/de-gnome/default.nix
+++ modules/home/de-gnome/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   imports = [
     ../desktop
   ];
   dconf = {
     enable = true;
-    settings = {
+    settings = with lib.hm.gvariant; {
       "org/gnome/shell" = {
         disable-extension-version-validation = true;
         favorite-apps = [
@@ -19,13 +19,31 @@
           "com.github.wwmm.easyeffects.desktop"
         ];
       };
+
       "org/gnome/desktop/interface" = {
         show-battery-percentage = true;
       };
+
       "org/gnome/mutter" = {
         edge-tiling = true;
         dynamic-workspaces = true;
+        workspaces-only-on-primary = true;
       };
+
+      "org/gnome/desktop/input-sources" = {
+        sources = [
+          (mkTuple [
+            "xkb"
+            "pl"
+          ])
+        ];
+        xkb-options = [
+          "terminate:ctrl_alt_bksp"
+          "lv3:ralt_switch"
+          "caps:swapescape"
+        ];
+      };
+
       "org/gnome/desktop/wm/keybindings" = {
         switch-windows = [ "<Alt>Tab" ];
         switch-windows-backward = [ "<Shift><Alt>Tab" ];
@@ -34,6 +52,7 @@
         switch-group = [ ];
         switch-group-backward = [ ];
       };
+
       "org/gnome/settings-daemon/plugins/color".night-light-enabled = true;
       "org/gnome/nautilus/preferences".click-policy = "single";
     };
