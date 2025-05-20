109bea1bdbdbbbfc4a28eb071c35feef16c9eebc
chtc <chtc@tuta.io>
2025-05-20 16:21:42 +0200

    feat(git): better git settings

--- modules/home/common/git.nix
+++ modules/home/common/git.nix
@@ -5,6 +5,48 @@
       userName = "chtc";
       userEmail = "chtc@tuta.io";
 
+      signing = {
+        format = "ssh";
+        key = "~/.ssh/id_ed25519";
+        signByDefault = true;
+      };
+
+      extraConfig = {
+        branch.sort = "-comitterdate";
+        column.ui = "auto";
+        commit.verbose = true;
+        help.autocorrect = "prompt";
+        init.defaultBranch = "main";
+        merge.conflictstyle = "zdiff3";
+        pull.rebase = true;
+        tag.sort = "version:refname";
+
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = true;
+          renames = true;
+        };
+
+        fetch = {
+          prune = true;
+          pruneTags = true;
+          all = true;
+        };
+
+        push = {
+          default = "simple";
+          autoSetupRemote = true;
+          followTags = true;
+        };
+
+        rebase = {
+          autoSquash = true;
+          autoStash = true;
+          updateRefs = true;
+        };
+      };
+
       aliases = {
         d = "diff";
         a = "add";
