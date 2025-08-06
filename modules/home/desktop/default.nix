8f7ee3d53a54b63d7bb7d33d7f21db94cb2ea855
chtc <chtc@tuta.io>
2025-08-06 14:26:59 +0200

    feat(vesktop): enable more plugins

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -43,6 +43,37 @@
     foliate.enable = true;
     keepassxc.enable = true;
     swayimg.enable = true;
-    vesktop.enable = true;
+    vesktop = {
+      enable = true;
+
+      settings = {
+        discordBranch = "canary";
+        hardwareAcceleration = true;
+      };
+      vencord.settings.plugins = {
+        BetterGifAltText.enabled = true;
+        BetterRoleContext.enabled = true;
+        BetterRoleDot.enabled = true;
+        BetterSessions.enabled = true;
+        BetterSettings.enabled = true;
+        BetterUploadButton.enabled = true;
+        ClearURLs.enabled = true;
+        CopyUserURLs.enabled = true;
+        FakeNitro.enabled = true;
+        FavoriteGifSearch.enabled = true;
+        FixSpotifyEmbeds.enabled = true;
+        FixYoutubeEmbeds.enabled = true;
+        ForceOwnerCrown.enabled = true;
+        ImageZoom.enabled = true;
+        MessageLogger.enabled = true;
+        PermissionsViewer.enabled = true;
+        PlatformIndicators.enabled = true;
+        RelationshipNotifier.enabled = true;
+        ShowHiddenChannels.enabled = true;
+        ShowHiddenThings.enabled = true;
+        SpotifyCrack.enabled = true;
+        YoutubeAdblock.enabled = true;
+      };
+    };
   };
 }
