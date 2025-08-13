40c239c63e5c7f237f63a51191f4a1f7487a3118
chtc <chtc@tuta.io>
2025-08-13 15:26:01 +0200

    feat(jujutsu): add tug alias and use ago timetamps

--- modules/home/common/vcs.nix
+++ modules/home/common/vcs.nix
@@ -1,9 +1,4 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
+{ config, pkgs, ... }:
 {
   home.packages = [ pkgs.watchman ];
 
@@ -93,6 +88,21 @@
             "git"
             "push"
           ];
+          tug = [
+            "util"
+            "exec"
+            "--"
+            "sh"
+            "-c"
+            ''
+              if [ "x$1" = "x" ]; then
+                jj bookmark move --from "closest_bookmark(@)" --to "closest_pushable(@)"
+              else
+                jj bookmark move --to "closest_pushable(@)" "$@"
+              fi
+            ''
+            ""
+          ];
         };
 
         git = {
@@ -103,17 +113,28 @@
           push = "origin";
         };
 
+        revset-aliases = {
+          "closest_bookmark(to)" = "heads(::to & bookmarks())";
+          "closest_pushable(to)" =
+            "heads(::to & mutable() & ~description(exact:\"\") & (~empty() | merges()))";
+        };
+
         signing = {
           behavior = "own";
           backend = "ssh";
           inherit (config.programs.git.signing) key;
         };
 
+        template-aliases = {
+          "format_timestamp(timestamp)" = "timestamp.ago()";
+        };
+
         ui = {
           default-command = "status";
           editor = config.home.sessionVariables.EDITOR;
+          diff-editor = ":builtin";
           diff-formatter = [
-            "${lib.getExe pkgs.difftastic}"
+            "difft"
             "--color=always"
             "$left"
             "$right"
