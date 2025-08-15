e5fd10f43899254ea82ae0a4f444a99fb67850fe
chtc <chtc@tuta.io>
2025-08-15 19:18:52 +0200

    refactor(niri): merge powermenu.sh into default.nix

--- modules/home/desktop/niri/default.nix
+++ modules/home/desktop/niri/default.nix
@@ -98,7 +98,24 @@
         hotkey-overlay.title = "Run an Application";
       };
       "Mod+Alt+E" = {
-        action = spawn "sh" "${./powermenu.sh}";
+        action = spawn "${pkgs.writeShellScript "powermenu" ''
+          SELECTION="$(printf "Lock\nSuspend\nLog out\nShutdown\nReboot\nReboot to UEFI\n" | fuzzel --dmenu -l 6)"
+
+          case $SELECTION in
+          	"Lock")
+          		loginctl lock-session;;
+          	"Suspend")
+          		systemctl suspend;;
+          	"Log out")
+          		niri msg action quit -s;;
+          	"Shutdown")
+          		systemctl poweroff;;
+          	"Reboot")
+          		systemctl reboot;;
+          	"Reboot to UEFI")
+          		systemctl reboot --firmware-setup;;
+          esac
+        ''}";
         hotkey-overlay.title = "Open Power Menu";
       };
       "Mod+Alt+L" = {
