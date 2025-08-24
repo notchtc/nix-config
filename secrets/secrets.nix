e19c44cb399d92ef463311ccce6df99ef87c34cc
chtc <chtc@tuta.io>
2025-08-24 04:20:57 +0200

    chore: update host ssh key (oops)

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,6 @@
 let
   user-chtc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtYPqw1VitvlRnvnO5skoX7vd+N6GET5XrFH4n3jNn3";
-  host-dorothy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHLvG8iW/LvGCX3uc3FvYRuJkQ8pqS4uNpbR2tBN1wf";
+  host-dorothy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHg8mQglY4N0IeWxICZWfwiKdhPsQn08wCqrnBYehtco";
 in
 {
   "chtc-password.age".publicKeys = [
