070b9537f0f1d3b4ca5097c6f94fb8cd33223cf5
chtc <chtc@tuta.io>
2025-09-19 20:56:19 +0200

    npins: use master

--- modules/nixos/environment/packages.nix
+++ modules/nixos/environment/packages.nix
@@ -1,15 +1,17 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  project,
+  system,
+  ...
+}:
 {
   environment = {
     defaultPackages = lib.mkForce [ ];
     systemPackages = lib.attrValues {
-      inherit (pkgs)
-        busybox
-        gitMinimal
-        npins
-        ragenix
-        ;
+      inherit (pkgs) busybox gitMinimal ragenix;
 
+      npins = project.inputs.npins.result { inherit pkgs system; };
       uutils = lib.hiPrio pkgs.uutils-coreutils-noprefix;
     };
   };
