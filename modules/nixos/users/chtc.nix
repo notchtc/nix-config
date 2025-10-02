3fd8861d9f8909156683eb185c367d6233ab778b
chtc <chtc@tuta.io>
2025-10-02 18:29:59 +0200

    modules/nixos/users/chtc: add ssh key to authorized keys

--- modules/nixos/users/chtc.nix
+++ modules/nixos/users/chtc.nix
@@ -10,6 +10,10 @@
     ]
     ++ lib.optionals config.programs.gamemode.enable [ "gamemode" ]
     ++ lib.optionals config.services.pipewire.enable [ "pipewire" ];
+
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtYPqw1VitvlRnvnO5skoX7vd+N6GET5XrFH4n3jNn3"
+    ];
   };
 
   home-manager.users.chtc = {
