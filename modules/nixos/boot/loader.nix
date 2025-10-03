b2f233aa08c008288c5e27fa69e340129799883f
chtc <chtc@tuta.io>
2025-10-03 18:31:43 +0200

    modules/nixos/boot/loader: add secure boot support and lower timeout

--- modules/nixos/boot/loader.nix
+++ modules/nixos/boot/loader.nix
@@ -1,12 +1,18 @@
+{ config, lib, ... }:
 {
+  options.mama.system.secure-boot.enable = lib.mkEnableOption ''
+    Enable secure boot in Limine
+  '';
+
   boot.loader = {
     limine = {
       enable = true;
       efiSupport = true;
       enableEditor = false;
       maxGenerations = 10;
+      secureBoot.enable = lib.mkIf config.mama.system.secure-boot.enable true;
     };
 
-    timeout = 2;
+    timeout = 1;
   };
 }
