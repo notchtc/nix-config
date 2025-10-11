335e1b5069c59da877ac2d22bfd8320635ddb5f8
chtc <chtc@tuta.io>
2025-10-11 11:48:56 +0200

    modules/nixos/users/chtc: only add to networkmanager group if it's used

--- modules/nixos/users/chtc.nix
+++ modules/nixos/users/chtc.nix
@@ -1,15 +1,18 @@
 { config, lib, ... }:
+let
+  inherit (lib) optionals;
+in
 {
   users.users.chtc = {
     isNormalUser = true;
     hashedPasswordFile = config.age.secrets.chtc-password.path;
 
     extraGroups = [
-      "networkmanager"
       "wheel"
     ]
-    ++ lib.optionals config.programs.gamemode.enable [ "gamemode" ]
-    ++ lib.optionals config.services.pipewire.enable [ "pipewire" ];
+    ++ optionals config.programs.gamemode.enable [ "gamemode" ]
+    ++ optionals config.networking.networkmanager.enable [ "networkmanager" ]
+    ++ optionals config.services.pipewire.enable [ "pipewire" ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtYPqw1VitvlRnvnO5skoX7vd+N6GET5XrFH4n3jNn3"
