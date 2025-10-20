958c3b0431dfa03657b669ca90608a372bfc0eaa
chtc <chtc@tuta.io>
2025-10-20 17:28:34 +0200

    modules/home/environment/variables: set SSH_ASKPASS_REQUIRE to prefer

--- modules/home/environment/variables.nix
+++ modules/home/environment/variables.nix
@@ -3,6 +3,7 @@
   home.sessionVariables = {
     PAGER = "less -FR";
     SYSTEMD_PAGERSECURE = "true";
+    SSH_ASKPASS_REQUIRE = "prefer";
     VISUAL = config.home.sessionVariables.EDITOR;
   };
 }
