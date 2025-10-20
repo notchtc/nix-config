4a1245250c76d66ddaa25f4a4cf639f21edf8c14
chtc <chtc@tuta.io>
2025-10-20 17:23:48 +0200

    modules/home/programs/ghostty: set scrollback limit and hide mouse while typing

--- modules/home/programs/ghostty.nix
+++ modules/home/programs/ghostty.nix
@@ -1,25 +1,35 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.mama.desktops;
 in
 {
-  home.sessionVariables.TERMINAL = "${pkgs.ghostty}/bin/ghostty";
-  programs.ghostty = {
-    enable = true;
-    settings = {
-      cursor-style = "bar";
-      font-family = "Sarasa Term J";
-      font-size = 11;
-      theme =
-        if cfg.gnome.enable then
-          "Adwaita Dark"
-        else if cfg.plasma.enable then
-          "Breeze"
-        else
-          "Gruvbox Dark Hard";
-      window-padding-x = 3;
-      window-padding-y = 3;
-      window-theme = "ghostty";
+  config = lib.mkIf config.mama.profiles.graphical.enable {
+    home.sessionVariables.TERMINAL = "${pkgs.ghostty}/bin/ghostty";
+    programs.ghostty = {
+      enable = true;
+      settings = {
+        cursor-style = "bar";
+        font-family = "Sarasa Term J";
+        font-size = 11;
+        theme =
+          if cfg.gnome.enable then
+            "Adwaita Dark"
+          else if cfg.plasma.enable then
+            "Breeze"
+          else
+            "Gruvbox Dark Hard";
+        window-padding-x = 3;
+        window-padding-y = 3;
+        window-theme = "ghostty";
+
+        scrollback-limit = 100 * 1024 * 1024;
+        mouse-hide-while-typing = true;
+      };
     };
   };
 }
