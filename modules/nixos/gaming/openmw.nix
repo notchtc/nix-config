0d5ba9fed111040b8072df3f16a91c05ab1e863d
chtc <chtc@tuta.io>
2025-10-23 17:34:46 +0200

    modules/nixos/gaming: disable openmw

--- modules/nixos/gaming/openmw.nix
+++ modules/nixos/gaming/openmw.nix
@@ -2,8 +2,8 @@
   config,
   lib,
   pkgs,
-  #  project,
-  #  system,
+  project,
+  system,
   ...
 }:
 {
@@ -13,13 +13,13 @@
 
   config.environment.systemPackages = lib.mkIf config.mama.profiles.gaming.openmw.enable (
     lib.attrValues {
-      #      inherit (project.inputs.openmw-nix.result.packages.${system})
-      #        delta-plugin
-      #        momw-configurator
-      #        openmw-validator
-      #        s3lightfixes
-      #        umo
-      #        ;
+      inherit (project.inputs.openmw-nix.result.packages.${system})
+        delta-plugin
+        momw-configurator
+        openmw-validator
+        s3lightfixes
+        umo
+        ;
 
       inherit (pkgs) openmw;
     }
