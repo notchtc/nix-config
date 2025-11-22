075660986696a4c868fb61b35f82e41701ae17ed
chtc <chtc@tuta.io>
2025-11-22 17:09:02 +0100

    modules/home/environment/xdg: refactor & set __GL_SHADER_DISK_CACHE_PATH

--- modules/home/environment/xdg/default.nix
+++ modules/home/environment/xdg/default.nix
@@ -1,52 +1,56 @@
 { config, ... }:
+let
+  inherit (config.home) homeDirectory;
+  inherit (config.xdg) cacheHome configHome dataHome;
+in
 {
   imports = [ ./associations.nix ];
 
   xdg = {
     enable = true;
-    cacheHome = "${config.home.homeDirectory}/.cache";
-    configHome = "${config.home.homeDirectory}/.config";
-    dataHome = "${config.home.homeDirectory}/.local/share";
-    stateHome = "${config.home.homeDirectory}/.local/state";
+    cacheHome = "${homeDirectory}/.cache";
+    configHome = "${homeDirectory}/.config";
+    dataHome = "${homeDirectory}/.local/share";
+    stateHome = "${homeDirectory}/.local/state";
     userDirs = {
       enable = true;
       createDirectories = true;
-      desktop = "${config.home.homeDirectory}/Desktop";
-      documents = "${config.home.homeDirectory}/Documents";
-      download = "${config.home.homeDirectory}/Downloads";
-      music = "${config.home.homeDirectory}/Media/Music";
-      pictures = "${config.home.homeDirectory}/Media/Pictures";
-      publicShare = "${config.home.homeDirectory}/Public/Share";
-      templates = "${config.home.homeDirectory}/Public/Templates";
-      videos = "${config.home.homeDirectory}/Media/Videos";
+      desktop = "${homeDirectory}/Desktop";
+      documents = "${homeDirectory}/Documents";
+      download = "${homeDirectory}/Downloads";
+      music = "${homeDirectory}/Media/Music";
+      pictures = "${homeDirectory}/Media/Pictures";
+      publicShare = "${homeDirectory}/Public/Share";
+      templates = "${homeDirectory}/Public/Templates";
+      videos = "${homeDirectory}/Media/Videos";
     };
 
     configFile = {
       "wgetrc" = {
         enable = true;
         text = ''
-          hsts-file="${config.xdg.cacheHome}/wget-hsts"
+          hsts-file="${cacheHome}/wget-hsts"
         '';
       };
     };
   };
 
+  xresources.path = "${configHome}/X11/Xresources";
   home = {
-    sessionVariables = {
-      _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${config.xdg.configHome}/java";
-      ANDROID_HOME = "${config.xdg.dataHome}/android";
-      ANDROID_USER_HOME = "${config.xdg.dataHome}/android";
-      CUDA_CACHE_PATH = "${config.xdg.cacheHome}/nv";
-      DVDCSS_CACHE = "${config.xdg.dataHome}/dvdcss";
-      GNUPGHOME = "${config.xdg.dataHome}/gnupg";
-      GTK2_RC_FILES = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-      INPUTRC = "${config.xdg.configHome}/readline/inputrc";
-      LESSHISTFILE = "${config.xdg.dataHome}/lesshst";
-      PULSECOOKIE = "${config.xdg.configHome}/pulse/cookie";
-      WGETRC = "${config.xdg.configHome}/wgetrc";
-      WINEPREFIX = "${config.xdg.dataHome}/wine";
+    sessionVariables = rec {
+      __GL_SHADER_DISK_CACHE_PATH = "${cacheHome}/nv";
+      _JAVA_OPTIONS = "-Djava.util.prefs.userRoot=${configHome}/java";
+      ANDROID_HOME = "${dataHome}/android";
+      ANDROID_USER_HOME = ANDROID_HOME;
+      CUDA_CACHE_PATH = __GL_SHADER_DISK_CACHE_PATH;
+      DVDCSS_CACHE = "${dataHome}/dvdcss";
+      GNUPGHOME = "${dataHome}/gnupg";
+      GTK2_RC_FILES = "${configHome}/gtk-2.0/gtkrc";
+      INPUTRC = "${configHome}/readline/inputrc";
+      LESSHISTFILE = "${dataHome}/lesshst";
+      PULSECOOKIE = "${configHome}/pulse/cookie";
+      WGETRC = "${configHome}/wgetrc";
+      WINEPREFIX = "${dataHome}/wine";
     };
   };
-
-  xresources.path = "${config.xdg.configHome}/X11/Xresources";
 }
