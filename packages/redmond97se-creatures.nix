8a5ac10ef6836e7007ab13f65d90f598b285d55c
chtc <chtc@tuta.io>
2025-11-26 18:58:18 +0100

    packages/redmond97se-creatures: update to 2.04

--- packages/redmond97se-creatures.nix
+++ packages/redmond97se-creatures.nix
@@ -9,15 +9,15 @@
         _7zz,
       }:
       let
-        version = "2.03";
+        version = "2.04";
       in
       stdenvNoCC.mkDerivation {
         pname = "redmond97se-creatures";
         inherit version;
 
         src = fetchurl {
-          url = "https://github.com/SliverXReal/Redmond97-SE/releases/download/Version_${version}/Redmond97-SE_v${version}_Themes_Only.7z";
-          hash = "sha256-LXL7UBlRMeKPejaE6X434XJ/SKEl5k1mrvyBUVqOtkI=";
+          url = "https://codeberg.org/Sliver_X/Redmond97-SE/releases/download/v${version}/Redmond97-SE_v${version}_Themes_Only.7z";
+          hash = "sha256-rf7WQBosHs7IMllnS4LIAoy+LHmByUTZzi/7Z3dddR4=";
         };
 
         unpackPhase = ''
