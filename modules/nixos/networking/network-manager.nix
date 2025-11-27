ae81d6f0ad88e715b1a15ecca93936636f99a4d0
chtc <chtc@tuta.io>
2025-11-27 18:34:30 +0100

    modules/nixos/networking/network-manager: enable everywhere, disable MAC randomization for ethernet

--- modules/nixos/networking/network-manager.nix
+++ modules/nixos/networking/network-manager.nix
@@ -1,23 +1,18 @@
 { config, lib, ... }:
-let
-  inherit (lib) mkIf;
-  server = config.mama.profiles.server.enable;
-in
 {
-  config = mkIf (!server) {
-    networking.networkmanager = {
-      enable = true;
-      dns = "systemd-resolved";
-      wifi = {
-        backend = "iwd";
-        powersave = true;
-      };
+  networking.networkmanager = {
+    enable = true;
+    ethernet.macAddress = lib.mkIf config.mama.profiles.server.enable "preserve";
+    dns = "systemd-resolved";
+    wifi = {
+      backend = "iwd";
+      powersave = true;
     };
+  };
 
-    environment.etc."NetworkManager/system-connections".source = "/persist/NetworkManager/";
-    systemd = {
-      services.NetworkManager-wait-online.enable = false;
-      tmpfiles.rules = [ "d /persist/NetworkManager 0755 root root - -" ];
-    };
+  environment.etc."NetworkManager/system-connections".source = "/persist/NetworkManager/";
+  systemd = {
+    services.NetworkManager-wait-online.enable = false;
+    tmpfiles.rules = [ "d /persist/NetworkManager 0755 root root - -" ];
   };
 }
