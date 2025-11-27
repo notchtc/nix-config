f72d812ae0cbc2b58c65df16c4df614e5e21f688
chtc <chtc@tuta.io>
2025-11-27 18:35:43 +0100

    hosts: remove dorothy

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,21 +1,6 @@
 { config }:
 {
   config.systems.nixos = {
-    dorothy = {
-      pkgs = config.inputs.nixpkgs.result.x86_64-linux;
-
-      modules = [
-        ../modules/nixos
-        ./dorothy
-      ];
-
-      args = {
-        host = "dorothy";
-        project = config;
-        system = "x86_64-linux";
-      };
-    };
-
     elisabeth = {
       pkgs = config.inputs.nixpkgs.result.x86_64-linux;
 
