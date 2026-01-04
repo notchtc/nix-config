ea97f8b381d29cc8d0b3e7b001b1d38195845927
chtc <chtc@tuta.io>
2026-01-04 16:45:27 +0100

    modules/nixos/nix/tools: remove diff

--- modules/nixos/nix/tools.nix
+++ modules/nixos/nix/tools.nix
@@ -1,31 +1,14 @@
-{
-  config,
-  inputs,
-  pkgs,
-  system,
-  ...
-}:
+{ inputs, system, ... }:
 {
   environment = {
     systemPackages = [
       inputs.nilla-cli.result.packages.default.result.${system}
       inputs.nilla-nixos.result.packages.default.result.${system}
-      pkgs.lix-diff
     ];
   };
 
   system = {
     disableInstallerTools = true;
     tools.nixos-rebuild.enable = true;
-
-    activationScripts.diff = {
-      supportsDryActivation = true;
-      text = ''
-        if [[ -e /run/current-system ]]; then
-          echo "comparing changes..."
-          ${pkgs.lix-diff}/bin/lix-diff --lix-bin ${config.nix.package}/bin /run/current-system "$systemConfig"
-        fi
-      '';
-    };
   };
 }
