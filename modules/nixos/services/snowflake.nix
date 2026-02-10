b3410b68d50a776e68ef50171ed205f86527ce7a
chtc <chtc@tuta.io>
2026-02-10 15:18:45 +0100

    modules/nixos/services/snowflake: bring back on laptop

--- modules/nixos/services/snowflake.nix
+++ modules/nixos/services/snowflake.nix
@@ -1,4 +1,10 @@
-{ config, lib, ... }:
+{ pkgs, ... }:
 {
-  services.snowflake-proxy.enable = lib.mkIf (!config.mama.profiles.laptop.enable) true;
+  services = {
+    udev.extraRules = ''
+      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="0",RUN+="${pkgs.systemd}/bin/systemctl stop snowflake-proxy.service"
+      SUBSYSTEM=="power_supply",ENV{POWER_SUPPLY_ONLINE}=="1",RUN+="${pkgs.systemd}/bin/systemctl start snowflake-proxy.service"
+    '';
+    snowflake-proxy.enable = true;
+  };
 }
