e583c778bb24bff83871de9a200a71a2a073841c
chtc <chtc@tuta.io>
2026-02-11 12:19:07 +0100

    modules/home/environment/stylix: use pipe operators

--- modules/home/environment/stylix.nix
+++ modules/home/environment/stylix.nix
@@ -18,14 +18,16 @@
 
     xdg.configFile.kdeglobals.source =
       let
-        themePackage = builtins.head (
-          builtins.filter (
+        themePackage =
+          builtins.head
+          <| builtins.filter (
             p: builtins.match ".*stylix-kde-theme.*" (baseNameOf p) != null
-          ) config.home.packages
-        );
-        colorSchemeSlug = lib.concatStrings (
-          lib.filter lib.isString (builtins.split "[^a-zA-Z]" config.lib.stylix.colors.scheme)
-        );
+          ) config.home.packages;
+
+        colorSchemeSlug =
+          lib.concatStrings
+          <| lib.filter lib.isString
+          <| builtins.split "[^a-zA-Z]" config.lib.stylix.colors.scheme;
       in
       "${themePackage}/share/color-schemes/${colorSchemeSlug}.colors";
   };
