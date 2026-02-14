b8eb098fb9ec04edd35a89a5dd1c5d18dbabdf90
chtc <chtc@tuta.io>
2026-02-14 14:06:58 +0100

    modules/home/nix: remove redundant use-xdg-base-directories

--- modules/home/nix.nix
+++ modules/home/nix.nix
@@ -1,11 +1,8 @@
 {
-  nix = {
-    settings.use-xdg-base-directories = true;
-    gc = {
-      automatic = true;
-      randomizedDelaySec = "15min";
-      options = "--delete-older-than 7d";
-    };
+  nix.gc = {
+    automatic = true;
+    randomizedDelaySec = "15min";
+    options = "--delete-older-than 7d";
   };
 
   services.home-manager.autoExpire = {
