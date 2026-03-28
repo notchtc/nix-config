6bcf5e02536c0d74b4df55cfbf11f9636ef5553b
chtc <chtc@tuta.io>
2026-03-28 20:34:57 +0100

    modules/nixos/security/run0: remove background

--- modules/nixos/security/run0.nix
+++ modules/nixos/security/run0.nix
@@ -1,8 +1,24 @@
-{ inputs, ... }:
+{
+  inputs,
+  pkgs,
+  system,
+  ...
+}:
 {
   imports = [ inputs.run0-sudo-shim.result.nixosModules.default ];
+
   security = {
     wrappers.su.enable = false;
-    run0-sudo-shim.enable = true;
+    run0-sudo-shim = {
+      enable = true;
+      package = pkgs.symlinkJoin {
+        name = "sudo";
+        paths = [ inputs.run0-sudo-shim.result.packages.${system}.default ];
+        nativeBuildInputs = [ pkgs.makeWrapper ];
+        postBuild = ''
+          wrapProgram $out/bin/sudo --add-flags "--run0-extra-arg=--background="
+        '';
+      };
+    };
   };
 }
