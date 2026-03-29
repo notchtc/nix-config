78ba71d5c6ad68660864629d8fb4065ae28e049d
chtc <chtc@tuta.io>
2026-03-29 20:53:30 +0200

    modules/nixos/environment/xdg-portals: set xdgOpenUsePortal to true

--- modules/nixos/environment/xdg-portals.nix
+++ modules/nixos/environment/xdg-portals.nix
@@ -7,15 +7,11 @@
 {
   xdg.portal = lib.mkIf config.mama.profiles.graphical.enable {
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-    config.niri = {
-      default = [
-        "gnome"
-        "gtk"
-      ];
-      "org.freedesktop.impl.portal.Access" = "gtk";
-      "org.freedesktop.impl.portal.Notification" = "gtk";
-      "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
-      "org.freedesktop.impl.portal.FileChooser" = "gtk";
-    };
+    xdgOpenUsePortal = true;
+
+    config.niri.default = [
+      "gnome"
+      "gtk"
+    ];
   };
 }
