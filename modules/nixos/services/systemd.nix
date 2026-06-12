7e28408878217dde1bbf506d1d8871347ce8e68a
chtc <chtc@tuta.io>
2026-06-12 17:20:05 +0200

    modules/nixos/services/systemd: use rfc 42 settings

--- modules/nixos/services/systemd.nix
+++ modules/nixos/services/systemd.nix
@@ -1,5 +1,4 @@
-{ config, lib, ... }:
-{
+{ config, lib, ... }: {
   systemd = lib.mkIf config.mama.profiles.graphical.enable {
     settings.Manager = {
       DefaultTimeoutStartSec = "15s";
@@ -8,11 +7,11 @@
       DefaultDeviceTimeoutSec = "15s";
     };
 
-    user.extraConfig = ''
-      DefaultTimeoutStartSec=15s
-      DefaultTimeoutStopSec=15s
-      DefaultTimeoutAbortSec=15s
-      DefaultDeviceTimeoutSec=15s
-    '';
+    user.settings.Manager = {
+      DefaultTimeoutStartSec = "15s";
+      DefaultTimeoutStopSec = "15s";
+      DefaultTimeoutAbortSec = "15s";
+      DefaultDeviceTimeoutSec = "15s";
+    };
   };
 }
