cda2f65fe02309aa2b8e70d1a07a85ed5f8b2c2e
chtc <chtc@tuta.io>
2026-06-26 20:45:03 +0200

    packages: replace depencencies with uutils

--- modules/packages.nix
+++ modules/packages.nix
@@ -7,8 +7,32 @@
       ...
     }:
     let
-      inherit (lib.modules) mkForce;
       inherit (lib.attrsets) attrValues optionalAttrs;
+      inherit (lib.lists) genList;
+      inherit (lib.modules) mkForce;
+      inherit (lib.strings) concatStringsSep stringLength;
+
+      coreutils-full-name =
+        "coreuutils-full"
+        + concatStringsSep "" (genList (_: "_") (stringLength pkgs.coreutils-full.version));
+
+      coreutils-name =
+        "coreuutils" + concatStringsSep "" (genList (_: "_") (stringLength pkgs.coreutils.version));
+
+      diffutils-name =
+        "diffuutils" + concatStringsSep "" (genList (_: "_") (stringLength pkgs.diffutils.version));
+
+      findutils-name =
+        "finduutils" + concatStringsSep "" (genList (_: "_") (stringLength pkgs.findutils.version));
+
+      hostname-name =
+        "hostname-uutils"
+        + concatStringsSep "" (genList (_: "_") ((stringLength pkgs.hostname-debian.version) + 1));
+
+      procps-name = "uprocps" + concatStringsSep "" (genList (_: "_") (stringLength pkgs.procps.version));
+
+      sed-name =
+        "uused" + concatStringsSep "" (genList (_: "_") ((stringLength pkgs.gnused.version) + 2));
     in
     {
       environment = {
@@ -97,5 +121,57 @@
       };
 
       programs.nano.enable = false;
+
+      system.replaceDependencies.replacements = [
+        {
+          oldDependency = pkgs.coreutils-full;
+          newDependency = pkgs.symlinkJoin {
+            name = coreutils-full-name;
+            paths = [ pkgs.uutils-coreutils-noprefix ];
+          };
+        }
+        {
+          oldDependency = pkgs.coreutils;
+          newDependency = pkgs.symlinkJoin {
+            name = coreutils-name;
+            paths = [ pkgs.uutils-coreutils-noprefix ];
+          };
+        }
+        {
+          oldDependency = pkgs.diffutils;
+          newDependency = pkgs.symlinkJoin {
+            name = diffutils-name;
+            paths = [ pkgs.uutils-diffutils ];
+          };
+        }
+        {
+          oldDependency = pkgs.findutils;
+          newDependency = pkgs.symlinkJoin {
+            name = findutils-name;
+            paths = [ pkgs.uutils-findutils ];
+          };
+        }
+        {
+          oldDependency = pkgs.hostname-debian;
+          newDependency = pkgs.symlinkJoin {
+            name = hostname-name;
+            paths = [ pkgs.uutils-hostname ];
+          };
+        }
+        {
+          oldDependency = pkgs.procps;
+          newDependency = pkgs.symlinkJoin {
+            name = procps-name;
+            paths = [ pkgs.uutils-procps ];
+          };
+        }
+        {
+          oldDependency = pkgs.gnused;
+          newDependency = pkgs.symlinkJoin {
+            name = sed-name;
+            paths = [ pkgs.uutils-sed ];
+          };
+        }
+      ];
     };
 }
