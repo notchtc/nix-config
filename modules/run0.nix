ee5e9dbf48e7c340e0c304acc2e473e30a3c6011
chtc <chtc@tuta.io>
2026-06-26 20:45:40 +0200

    run0: use the nixos module

--- modules/run0.nix
+++ modules/run0.nix
@@ -1,16 +1,14 @@
 {
-  config.modules.nixos.run0 = { inputs, pkgs, ... }: {
-    environment.systemPackages = [
-      #  pkgs.run0-sudo-shim
-      inputs.run0-sudo-shim.result.packages.${pkgs.stdenv.hostPlatform.system}.default
-    ];
-
+  config.modules.nixos.run0 = {
     security = {
-      #      run0.enable = true;
+      run0 = {
+        enable = true;
+        sudo-shim.enable = true;
+      };
 
       sudo.enable = false;
 
-      #      polkit.enablePkexecWrapper = false;
+      polkit.enablePkexecWrapper = false;
       wrappers = {
         su.enable = false;
         sg.enable = false;
