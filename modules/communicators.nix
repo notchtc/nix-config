989c4617f763a6da4aa377a67022902275987cce
chtc <chtc@tuta.io>
2026-07-04 19:28:21 +0200

    communicators: more vencord plugins and slight config changes

--- modules/communicators.nix
+++ modules/communicators.nix
@@ -17,13 +17,23 @@
               value = {
                 arRPC = true;
                 discordBranch = "canary";
+                enableSplashScreen = false;
                 hardwareAcceleration = true;
+
+                spellCheckLanguagues = [
+                  "en-US"
+                  "pl"
+                ];
               };
             };
 
             "vesktop/settings/settings.json" = {
               generator = toJSON { };
               value = {
+                useQuickCss = false;
+
+                themeLinks = [ "https://croissantdunord.github.io/discord-adblock/adblock.css" ];
+
                 plugins = {
                   BetterGifAltText.enabled = true;
                   BetterRoleContext.enabled = true;
@@ -33,19 +43,28 @@
                   BetterUploadButton.enabled = true;
                   ClearURLs.enabled = true;
                   CopyUserURLs.enabled = true;
+                  CrashHandler.enabled = true;
                   FakeNitro.enabled = true;
                   FavoriteGifSearch.enabled = true;
                   FixSpotifyEmbeds.enabled = true;
                   FixYoutubeEmbeds.enabled = true;
                   ForceOwnerCrown.enabled = true;
                   ImageZoom.enabled = true;
+                  ImplicitRelationships.enabled = true;
+                  MessageLinkEmbeds.enabled = true;
                   MessageLogger.enabled = true;
+                  NoMaskedUrlPaste.enabled = true;
+                  NoMiddleClickPaste.enabled = true;
                   PermissionsViewer.enabled = true;
                   PlatformIndicators.enabled = true;
                   RelationshipNotifier.enabled = true;
                   ShowHiddenChannels.enabled = true;
                   ShowHiddenThings.enabled = true;
                   SpotifyCrack.enabled = true;
+                  ValidReply.enabled = true;
+                  ValidUser.enabled = true;
+                  WebKeybinds.enabled = true;
+                  WebScreenShareFixes.enabled = true;
                   YoutubeAdblock.enabled = true;
                 };
               };
