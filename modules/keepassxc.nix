b8b34e5ffd59db41bfd33accff7f20b496f11e61
chtc <chtc@tuta.io>
2026-07-06 17:36:44 +0200

    keepassxc: remove systemd service

--- modules/keepassxc.nix
+++ modules/keepassxc.nix
@@ -1,39 +1,24 @@
 {
-  config.modules.home.keepassxc =
-    { lib, pkgs, ... }:
-    let
-      inherit (lib.meta) getExe;
-    in
-    {
-      rum.programs.keepassxc = {
-        enable = true;
+  config.modules.home.keepassxc = {
+    rum.programs.keepassxc = {
+      enable = true;
 
-        settings = {
-          GUI = {
-            ApplicationTheme = "dark";
-            MinimizeOnClose = true;
-            MinimizeOnStartup = true;
-            ShowTrayIcon = true;
-            ToolButtonStyle = 4;
-            TrayIconAppearance = "monochrome-light";
-          };
-
-          Security = {
-            ClearSearch = true;
-            HideTotpPreviewPanel = true;
-          };
-
-          Browser.Enabled = true;
-          General.ConfigVersion = 2;
+      settings = {
+        GUI = {
+          MinimizeOnClose = true;
+          ShowTrayIcon = true;
+          ToolButtonStyle = 4;
+          TrayIconAppearance = "monochrome-light";
         };
-      };
 
-      systemd.services.keepassxc = {
-        description = "KeepassXC";
-        after = [ "graphical-session.target" ];
-        wantedBy = [ "graphical-session.target" ];
+        Security = {
+          ClearSearch = true;
+          HideTotpPreviewPanel = true;
+        };
 
-        serviceConfig.ExecStart = getExe pkgs.keepassxc;
+        Browser.Enabled = true;
+        General.ConfigVersion = 2;
       };
     };
+  };
 }
