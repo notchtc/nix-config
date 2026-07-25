34c925d730d6a7529302c817dcf58ac8a7cf8daa
chtc <chtc@tuta.io>
2026-07-25 16:05:37 +0200

    memory: get rid of anything oom related from the module

--- modules/memory.nix
+++ modules/memory.nix
@@ -1,73 +1,15 @@
 {
-  config.modules.nixos.memory =
-    { config, lib, ... }:
-    let
-      inherit (lib.modules) mkIf;
-      inherit (lib.strings) concatStringsSep;
-    in
-    {
-      zramSwap = {
-        enable = true;
-        memoryPercent = 150;
-      };
-
-      boot.kernel.sysctl = {
-        "vm.page-cluster" = 0;
-        "vm.swappiness" = 180;
-        "vm.watermark_boost_factor" = 0;
-        "vm.watermark_scale_factor" = 125;
-      };
-
-      systemd.oomd = {
-        enableRootSlice = true;
-        enableSystemSlice = true;
-        enableUserSlices = true;
-        settings.OOM.DefaultMemoryPressureDurationSec = "20s";
-      };
-
-      services.earlyoom =
-        let
-          avoid = concatStringsSep "|" [
-            "dbus-.*"
-            "ghostty"
-            "greetd"
-            "hx"
-            "niri"
-            "noctalia"
-            "ssh-agent"
-            "sshd"
-            "systemd-.*"
-            "systemd"
-            "Xwayland"
-            "xwayland-satellite"
-            "zsh"
-          ];
-
-          prefer = concatStringsSep "|" [
-            ".*.exe"
-            "Isolated Web Co"
-            "Web Content"
-            "Web Extensions"
-            "chrom(e|ium).*"
-            "electron"
-            "firefox.*"
-            "java.*"
-            "librewolf.*"
-            "nix"
-            "pipewire(.*)"
-          ];
-        in
-        mkIf config.xdg.portal.enable {
-          enable = true;
-          freeSwapThreshold = 2;
-          freeMemThreshold = 2;
-          extraArgs = [
-            "-g"
-            "--avoid"
-            "'^(${avoid})$'"
-            "--prefer"
-            "'^(${prefer})$'"
-          ];
-        };
+  config.modules.nixos.memory = {
+    zramSwap = {
+      enable = true;
+      memoryPercent = 150;
     };
+
+    boot.kernel.sysctl = {
+      "vm.page-cluster" = 0;
+      "vm.swappiness" = 180;
+      "vm.watermark_boost_factor" = 0;
+      "vm.watermark_scale_factor" = 125;
+    };
+  };
 }
