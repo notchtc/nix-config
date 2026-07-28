e1ce5197ed27537e2b714a810c8b666415055489
chtc <chtc@tuta.io>
2026-07-28 15:33:19 +0200

    nix: remove nix-cachyos-kernel cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -54,9 +54,6 @@
             "cgroups"
             "pipe-operator"
           ];
-
-          substituters = [ "https://attic.xuyh0120.win/lantian" ];
-          trusted-public-keys = [ "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc=" ];
         };
 
         gc = {
